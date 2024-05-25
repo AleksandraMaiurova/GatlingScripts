81dc3a00a5a5467ecd6e90d969ae8017a88043dd
Aleksandra Maiurova <asmaiurova@google.com>
2024-05-25 22:49:57 +0300

    webtourstest

--- src/test/scala/webTourstest/Debug.scala
+++ src/test/scala/webTourstest/Debug.scala
@@ -6,10 +6,15 @@ import io.gatling.http.Predef._
 class Debug extends Simulation{
 
   setUp(CommonScenario().inject(
-    rampConcurrentUsers(1).to(30).during(1200)  ))
+    constantConcurrentUsers(56).during(3600)
+      ))
     .protocols(httpProtocol)
-    .assertions(global.successfulRequests.percent.gt(90))
+    .assertions(forAll.responseTime.max.lt(3000), global.successfulRequests.percent.gt(90))
     .maxDuration(1800000)
 
-
+//  incrementConcurrentUsers(5)
+//    .times(30)
+//    .eachLevelLasting(10)
+//    .separatedByRampsLasting(10)
+//    .startingFrom(0)
 }
