49de1cd30d8baee862dbfb9840e4c3a6924cf347
Aleksandra Maiurova <asmaiurova@google.com>
2024-05-29 13:22:28 +0300

    refactor

--- src/test/scala/webTourstest/Debug.scala
+++ src/test/scala/webTourstest/Debug.scala
@@ -6,15 +6,21 @@ import io.gatling.http.Predef._
 class Debug extends Simulation{
 
   setUp(CommonScenario().inject(
-    constantConcurrentUsers(56).during(3600)
-      ))
+      incrementUsersPerSec((1).toInt)
+        // Количество ступеней
+        .times(7)
+        // Длительность полки
+        .eachLevelLasting(25)
+        // Длительность разгона
+        .separatedByRampsLasting(5)
+        // Начало нагрузки с
+        .startingFrom(0),
+
+  ))
     .protocols(httpProtocol)
     .assertions(forAll.responseTime.max.lt(3000), global.successfulRequests.percent.gt(90))
     .maxDuration(1800000)
 
-//  incrementConcurrentUsers(5)
-//    .times(30)
-//    .eachLevelLasting(10)
-//    .separatedByRampsLasting(10)
-//    .startingFrom(0)
+
+//  constantConcurrentUsers(56).during(3600)
 }
